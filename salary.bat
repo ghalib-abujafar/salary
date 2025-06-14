5197dd59a875a5322f1d2efcd21114dc90836cdd
ghalib-abujafar <112585245+ghalib-abujafar@users.noreply.github.com>
2025-06-14 17:38:41 +0300

    Add files via upload

---
+++ salary.bat
@@ -0,0 +1,5 @@
+@echo off
+call C:\Users\ghali\anaconda3\Scripts\activate.bat base
+cd /d ""D:\salary\sum""
+python sum_salary.py
+pause
---
+++ sum_salary.py
@@ -0,0 +1,29 @@
+
+li=[]
+while True:
+    x, y = map(int, input("\033[92menter the hour and minutes: ").split())
+    if x == -1 or y == -1:
+        break
+    li.append([x,y])
+minutes=sum(int(row[1]) for row in li)
+hour=sum(int(row[0]) for row in li)+ int(minutes/60)
+
+salary = (hour + ((minutes%60) / 60 ))*1.7
+
+has_double=input("do you have hour on double pay Yes or No: ")
+if has_double.lower() == "yes":
+    d_hour = 0
+    d_minutes = 0
+    while True:
+        h, m=map(int, input("enter the hour and minutes: ").split())
+        if h == -1 or m == -1:
+            break
+        d_hour = d_hour + h
+        d_minutes = d_minutes + m
+        
+    double_pay=((d_hour/2)+((d_minutes%60) / 60 ))*1.7
+    salary = salary + double_pay
+
+print(f"\033[93myour hours is {hour}h and {minutes%60}m")
+print(f"\033[91myour salary without Daman is : {salary:.2f}")
+print(f"\033[94myour salary with Daman is : {salary-23:.2f}\033[0m ")
